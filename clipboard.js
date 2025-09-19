dd1be000e3311207f068585ecf32eac30e6be2d1
Jortoo <spruit51@gmail.com>
2025-09-19 19:54:07 +0200

    Added some small features

--- clipboard.js
+++ clipboard.js
@@ -1,3 +1,7 @@
+let aboutMeText = "Ik ben Jordi, 19 jaar en ik heb een passie voor alles dat met computers temaken heeft. Waaronder coderen, hardware en systeem beheer. Leuk dat je een kijkje neemt op mijn website. Deze website is nu nog in onderhoud dus kom later terug!"
+let currentAboutMeText = ""
+const aboutMeTextDisplay = document.getElementById("about-me")
+
 function getButton(id) { 
     const button = document.getElementById(id);
     if (!button) {
@@ -5,6 +9,22 @@ function getButton(id) {
     }
     return button;
 }
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function displayAboutMeText() {
+
+    for (i = 0; i < aboutMeText.length; i++) {
+        currentAboutMeText += aboutMeText.charAt(i);
+        aboutMeTextDisplay.textContent = currentAboutMeText;
+        await wait(25);
+    }
+
+}
+
+displayAboutMeText()
 
 getButton("mail").onclick = function() {
     window.location.href = "mailto:spruit51@gmail.com"
