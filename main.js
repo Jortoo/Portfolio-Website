f2dfe47b1c0cb74c577bfe0b5950473d5a55e325
Jortoo <jqrtox@gmail.com>
2025-09-19 21:41:18 +0200

    Fixed a bug and some translation issues

--- main.js
+++ main.js
@@ -26,17 +26,13 @@ async function displayAboutMeText() {
 
 displayAboutMeText()
 
-getButton("mail").onclick = function() {
-    window.location.href = "mailto:spruit51@gmail.com"
-}
-getButton("github").onclick = function() {
-    window.location.href ="https://github.com/Jortoo";
-}
+getButton("mail").onclick = function() { window.location.href = "mailto:jortoo@gmail.com" }
+getButton("contact").onclick = function() { window.location.href = "mailto:jortoo@gmail.com" }
+getButton("github").onclick = function() { window.location.href ="https://github.com/Jortoo"; }
 getButton("discord").onclick = function() {
     navigator.clipboard.writeText("Jortoo");
     alert("Discord tag copied to clipboard!");
 }
-getButton("home").onclick = function() {
-    window.location.href = "index.html";
-}
+
+getButton("home").onclick = function() { window.location.href = "index.html"; }
 
